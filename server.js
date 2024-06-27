42dd4dfdf9b1f73b27096adc0b96cb996f8a9c6f
yashow3101 <yashow3101@gmail.com>
2024-06-27 10:28:15 +0530

    Commit

--- server.js
+++ server.js
@@ -8,32 +8,70 @@ const cors = require('cors')
 app.use(bodyparser.json());
 app.use(cors());
 
+const newData1 = {"type": "live",
+    "id": "place.29403243",
+    "name": "Mumbai",
+    "address": "Mumbai, Maharashtra, India",
+    "longitude": 72.878723,
+    "latitude": 19.077793}
+
+const newData2 = {"type": "live",
+    "id": "place.29403243",
+    "name": "New Delhi",
+    "address": "Mumbai, Maharashtra, India",
+    "longitude": 72.878723,
+    "latitude": 19.077793}
+
+const newData3 = {"type": "live",
+    "id": "place.29403243",
+    "name": "Pune",
+    "address": "Mumbai, Maharashtra, India",
+    "longitude": 72.878723,
+    "latitude": 19.077793}
+
+    const initializeData = () => {
+        fs.readFile('data.json', (err, data) => {
+            let locations = { live: [], inProgress: [], demo: [] };
+    
+            if (!err) {
+                try {
+                    locations = JSON.parse(data);
+                } catch (parseErr) {
+                    console.error('Error parsing data file:', parseErr);
+                }
+            }
+    
+            locations.live = locations.live || [];
+            locations.inProgress = locations.inProgress || [];
+            locations.demo = locations.demo || [];
+    
+            locations.live.push(newData1);
+            locations.inProgress.push(newData2);
+            locations.demo.push(newData3);
+    
+            const updatedData = JSON.stringify(locations, null, 2);
+            fs.writeFile('data.json', updatedData, err => {
+                if (err) throw err;
+                console.log('Initial data added successfully');
+            });
+        });
+    };
+
+initializeData();
+
 app.post('/addLocation', (req, res) => {
     const newLocation = req.body;
 
     fs.readFile('data.json', (err, data) => {
         if (err) throw err;
 
+        // locations = { live: [], inProgress: [], demo: [] };
         let locations;
         try {
             locations = JSON.parse(data);
         } catch (parseErr) {
             console.error('Error parsing data file:', parseErr);
             return res.status(500).send('Internal Server Error');
-        }
-
-        if (!locations || typeof locations !== 'object') {
-            locations = { live: [], inProgress: [], demo: [] };
-        }
-
-        if (!Array.isArray(locations.live)) {
-            locations.live = [];
-        }
-        if (!Array.isArray(locations.inProgress)) {
-            locations.inProgress = [];
-        }
-        if (!Array.isArray(locations.demo)) {
-            locations.demo = [];
         }
 
         switch (newLocation.type) {
